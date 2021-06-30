3d3edfd2f735caefb114d5895568cf4b70f58d1e
Flinner <flinner.hlqn@aleeas.com>
2021-06-30 09:12:23 +0300

    feat: no feat

--- hack_assembler.v
+++ hack_assembler.v
@@ -3,17 +3,36 @@ module main
 import os
 import term
 
+type AString = []string
+
+type DeferredFn = fn (mut AString, int, AInstruction, mut Symbols)
+
+struct Deferred {
+mut:
+	i           int
+	instruction AInstruction
+	symbols     Symbols
+	asm_lines   AString
+}
+
 fn main() {
 	term.erase_clear() // tmp
-
-	mut symbols := init_symbols()
-
 	settings := handle_args()
-	println('filename1: $settings.filename\n')
-	lines := os.read_lines(settings.filename) or { panic("can't read file") }
 
+	lines := os.read_lines(settings.filename) or { panic("can't read file") }
 	mut asm_lines := []string{len: lines.len}
+
+	defer_hack(mut asm_lines, lines)
+
+	println('asm_lines ${asm_lines.join('\n')}')
+}
+
+fn defer_hack(mut asm_lines []string, lines []string) {
+	mut symbols := init_symbols()
 	mut i := 0
+
+	// mut defered_functions := []fn ([]string, int, AInstruction, Symbols) {len:lines.len}
+	mut deferred_functions := []Deferred{len: 9}
 
 	for line in lines {
 		// Returns Mobject
@@ -32,14 +51,27 @@ fn main() {
 				// 1. its is a predefined symbols, (R1,SCREEN,..), just return the map
 				// 2. its is a variable (1,2,3...), return it
 				// 3. new symbol, write it to the symbols map, return its value
-				defer {
-					asm_lines[i] = parsed.to_asm(mut symbols)
+				println('to be deferred')
+				// asm_lines[i] = parsed.to_asm(mut symbols)
+
+				deferred_functions << Deferred{
+					i: i
+					asm_lines: &mut asm_lines
+					instruction: parsed
+					symbols: mut symbols
 				}
 			}
 			else {}
 		}
 		i++
 	}
+	for mut deferred in deferred_functions {
+		println(deferred.asm_lines)
+		deferred.asm_lines[deferred.i] = 'deferred'
+	}
 	// println(asm_lines.join('\n'))
 	// println(asm_lines)
+}
+
+fn real_main(mut asm_lines []string, lines []string) {
 }
